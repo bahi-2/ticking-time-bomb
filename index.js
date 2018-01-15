43239533947fb93b3830746f16dd51b9d0bf22c8
Blaz <blaz.bagic@gmail.com>
2018-01-15 20:14:27 +0100

    Added test for loop.

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+	.use(express.static(path.join(__dirname, 'public')))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/', (req, res) => res.render('pages/index'))
+	.get('/times', function(request, response) {
+		var result = ''
+		var times = process.env.TIMES || 5
+		for (i = 0; i < times; i++)
+			result += i + ' ';
+		response.send(result);
+	})
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
